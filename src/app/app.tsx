1032c20956cd3e286378eed688c5a069dba57fdf
developerlucaslima <developerlucaslima@gmail.com>
2025-08-13 14:39:26 -0300

    feat: integrate React Query for data fetching
    - Added QueryClientProvider to the main application component for managing server state.
    - Created a new file to initialize the QueryClient instance for use throughout the application.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,19 +1,19 @@
-// import { QueryClientProvider } from '@tanstack/react-query'
 // import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from '@tanstack/react-query'
 import { ThemeProvider } from 'styled-components'
 
+import { queryClient } from '@/shared/lib/react-query'
 import { GlobalStyle } from '@/shared/styles/global'
 import { theme } from '@/shared/styles/themes'
-// import { queryClient } from './lib/react-query'
-// import { router } from './routes/router'
+
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      {/* <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider> */}
+      <QueryClientProvider client={queryClient}>
+        {/* <RouterProvider router={router} /> */}
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }

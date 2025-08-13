f6476e1441604f0a777bdf9a89eca0e880048136
developerlucaslima <developerlucaslima@gmail.com>
2025-08-13 16:34:09 -0300

    feat: enable routing in the application
    - Integrated RouterProvider from react-router-dom into the main application component to establish routing functionality.
    - Updated the app structure to support navigation between different pages.

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,8 @@
-// import { RouterProvider } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
+import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
+import { router } from '@/routes/router'
 import { queryClient } from '@/shared/lib/react-query'
 import { GlobalStyle } from '@/shared/styles/global'
 import { theme } from '@/shared/styles/themes'
@@ -12,7 +13,7 @@ export const App = () => {
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <QueryClientProvider client={queryClient}>
-        {/* <RouterProvider router={router} /> */}
+        <RouterProvider router={router} />
       </QueryClientProvider>
     </ThemeProvider>
   )

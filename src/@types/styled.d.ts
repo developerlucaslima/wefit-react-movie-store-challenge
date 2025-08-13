591999ccb5cfd635ac9dcf20b63b483ec168945c
developerlucaslima <developerlucaslima@gmail.com>
2025-08-13 14:22:15 -0300

    feat: add styled-components theme definitions
    - Introduced TypeScript definitions for styled-components theme, enhancing type safety for theme properties.
    - Created a theme object with color, border, breakpoint, and typography settings for consistent styling across the application.

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,40 @@
+import 'styled-components'
+
+declare module 'styled-components' {
+    export interface DefaultTheme {
+    colors: {
+      background: string
+      foreground: string
+      cardBackground: string
+      cardForeground: string
+      buttonBackground: string
+      buttonForeground: string
+      successBackground: string
+      successForeground: string
+      mutedForeground: string
+      border: string
+      ring: string
+    }
+    border: {
+      'rounded-full': string
+      'rounded-sm': string
+    }
+    breakpoints: {
+      sm: string
+      xl: string
+    }
+    typography: {
+      fontSize: {
+        xs: string
+        sm: string
+        md: string
+        lg: string
+        xl: string
+      }
+      fontWeight: {
+        semibold: number
+        bold: number
+      }
+    }
+  }
+}
---
+++ src/shared/styles/themes.ts
@@ -0,0 +1,38 @@
+import type { DefaultTheme } from 'styled-components/dist/types'
+
+export const theme: DefaultTheme = {
+  colors: {
+    background: '#2F2E41',
+    foreground: '#FFFFFF',
+    cardBackground: '#FFFFFF',
+    cardForeground: '#2F2E41',
+    buttonBackground: '#009EDD',
+    buttonForeground: '#FFFFFF',
+    successBackground: '#039B00',
+    successForeground: '#FFFFFF',
+    mutedForeground: '#999999',
+    border: '#D9D9D9',
+    ring: '#D9D9D9',
+  },
+  border: {
+    'rounded-full': '9999px',
+    'rounded-sm': '4px',
+  },
+  breakpoints: {
+    sm: '360px',
+    xl: '1440px',
+  },
+  typography: {
+    fontSize: {
+      xs: '12px',
+      sm: '14px',
+      md: '16px',
+      lg: '20px',
+      xl: '24px',
+    },
+    fontWeight: {
+      semibold: 600,
+      bold: 700,
+    },
+  },
+}

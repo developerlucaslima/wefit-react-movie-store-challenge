b7d9722cd777aa9b724e80e873b6937e1ef22583
developerlucaslima <developerlucaslima@gmail.com>
2025-08-14 18:24:47 -0300

    refactor: simplify router configuration by removing Suspense wrapper
    - Replaced lazy loading with direct imports for HomePage, CartPage, and CheckoutPage components in the router configuration.
    - Removed the Suspense wrapper around route elements to streamline the routing setup and improve code clarity.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,22 +1,17 @@
-import { type JSX,lazy, Suspense } from 'react'
 import { createBrowserRouter } from 'react-router-dom'
 
 import { MainLayout } from '@/app/layouts/app-layout/app-layout'
-
-
-const HomePage = lazy(() => import('@/app/modules/home/pages/home-page/home-page'))
-const CartPage = lazy(() => import('@/app/modules/cart/pages/cart-page/cart-page'))
-const CheckoutPage = lazy(() => import('@/app/modules/checkout/pages/checkout-page/checkout-page'))
-
-const wrap = (el: JSX.Element) => <Suspense fallback={null}>{el}</Suspense>
+import { CartPage } from '@/app/modules/cart/pages/cart-page/cart-page'
+import { CheckoutPage } from '@/app/modules/checkout/pages/checkout-page/checkout-page'
+import { HomePage } from '@/app/modules/home/pages/home-page/home-page'
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
-      { index: true, element: wrap(<HomePage />) },
-      { path: 'cart', element: wrap(<CartPage />) },
-      { path: 'checkout', element: wrap(<CheckoutPage />) },
+      { index: true, element: <HomePage /> },
+      { path: 'cart', element: <CartPage /> },
+      { path: 'checkout', element: <CheckoutPage /> },
     ],
   },
 ])

464b7284dc626951f1be2be3cadddc8755c047b5
developerlucaslima <developerlucaslima@gmail.com>
2025-08-14 10:40:42 -0300

    chore: remove Prettier configuration and integrate Prettier with ESLint
    - Deleted the .prettierrc file to streamline configuration management.
    - Updated eslint.config.js to include Prettier as a plugin and added Prettier rules to ESLint.
    - Added Prettier and ESLint dependencies in package.json and package-lock.json for improved code formatting.

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import { globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
+import pluginPrettier from 'eslint-plugin-prettier'
 
 export default tseslint.config([
   globalIgnores(['dist', 'node_modules']),
@@ -15,6 +17,7 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier,
     ],
     languageOptions: {
       ecmaVersion: 'latest',
@@ -22,6 +25,7 @@ export default tseslint.config([
     },
     plugins: {
       'simple-import-sort': simpleImportSort,
+      prettier: pluginPrettier,
     },
     rules: {
       // HMR safety
@@ -31,11 +35,28 @@ export default tseslint.config([
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error',
 
-      // Keep ESLint focused on logic; avoid stylistic rules that Prettier will handle
+      // Logic rules
       'no-console': ['warn', { allow: ['error', 'warn'] }],
       'no-debugger': 'error',
       'no-unused-vars': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+
+      // Prettier como regra
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 80,
+          tabWidth: 2,
+          singleQuote: true,
+          trailingComma: 'all',
+          arrowParens: 'always',
+          semi: false,
+          endOfLine: 'auto',
+        },
+      ],
     },
   },
 ])

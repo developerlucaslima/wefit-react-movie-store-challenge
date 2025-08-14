d0a361332a49aca3abf0d9d9a915f1503a902acb
developerlucaslima <developerlucaslima@gmail.com>
2025-08-14 14:09:57 -0300

    style: update MovieCard styles for improved layout and responsiveness
    - Adjusted width of the card for better alignment and consistency across breakpoints.
    - Added center alignment for card title and price for enhanced visual appeal.
    - Introduced media query for responsive width adjustment on larger screens.

--- src/app/modules/home/components/movie-card/movie-card-styles.ts
+++ src/app/modules/home/components/movie-card/movie-card-styles.ts
@@ -5,12 +5,18 @@ export const CardRoot = styled.div`
   color: ${({ theme }) => theme.colors.cardForeground};
   border-radius: ${({ theme }) => theme.border['rounded-sm']};
   padding: 16px;
+
   display: flex;
   flex-direction: column;
   gap: 8px;
+  align-items: center;
 
-  width: 338.67px;
+  width: 328px;
   height: 324px;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    width: 338.67px;
+  }
 `
 
 export const CardImage = styled.img`
@@ -26,12 +32,14 @@ export const CardTitle = styled.h2`
   font-size: ${({ theme }) => theme.typography.fontSize.xs};
   font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
   color: ${({ theme }) => theme.colors.strongForeground};
+  text-align: center;
 `
 
 export const CardPrice = styled.p`
   margin: 0;
   font-size: ${({ theme }) => theme.typography.fontSize.md};
   font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+  text-align: center;
 `
 
 export const CardActions = styled.div`

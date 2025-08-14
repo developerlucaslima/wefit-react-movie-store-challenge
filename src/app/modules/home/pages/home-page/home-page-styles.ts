f467e43a65c2341ba4deacf29dcc99af07816a59
developerlucaslima <developerlucaslima@gmail.com>
2025-08-14 10:41:58 -0300

    feat: implement HomePage component with movie listing and cart functionality
    - Created a new HomePage component that fetches and displays a list of movies using the useMovies hook.
    - Integrated a styled wrapper for the movie list layout.
    - Added functionality to add movies to the cart with a button that shows the quantity of items in the cart.
    - Utilized existing MovieCard components for displaying movie details.

---
+++ src/app/modules/home/pages/home-page/home-page-styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const MoviesListWrapper = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 24px 16px;
+  padding: 16px;
+`
--- src/app/modules/home/pages/home-page/home-page.tsx
+++ src/app/modules/home/pages/home-page/home-page.tsx
@@ -1,7 +1,57 @@
-const HomePage = () => {
+import { Button, ButtonIconCount } from '@/app/ui/button/button'
+import { useMovies } from '@/shared/api/get-all-movies/use-movies'
+import cartIcon from '@/shared/assets/icons/cart-icon.svg'
+import { useCart } from '@/shared/stores/cart-store/cart-store'
+
+import {
+  MovieCardActions,
+  MovieCardImage,
+  MovieCardPrice,
+  MovieCardRoot,
+  MovieCardTitle,
+} from '../../components/movie-card/movie-card'
+import { MoviesListWrapper } from './home-page-styles'
+
+export default function HomePage() {
+  const { data: movies = [], isLoading, isError } = useMovies()
+  const addItem = useCart((s) => s.addItem)
+  const items = useCart((s) => s.items)
+
+  // if (isLoading) return <Page>Carregando…</Page>
+  // if (isError) return <Page>Erro ao carregar filmes.</Page>
+
   return (
-    <h1>Home page</h1>
+    <MoviesListWrapper aria-label="Lista de filmes">
+      {movies.map((m) => {
+        const qty = items[m.id]?.quantity ?? 0
+        const isAdded = qty > 0
+        const titleId = `movie-${m.id}-title`
+
+        return (
+          <MovieCardRoot key={m.id} labelledBy={titleId}>
+            <MovieCardImage src={m.image} alt={`Capa do filme ${m.title}`} />
+            <MovieCardTitle id={titleId}>{m.title}</MovieCardTitle>
+            <MovieCardPrice amount={m.price} />
+
+            <MovieCardActions>
+              <Button
+                variant={isAdded ? 'success' : 'primary'}
+                onClick={() =>
+                  addItem({
+                    id: m.id,
+                    title: m.title,
+                    image: m.image,
+                    price: m.price,
+                  })
+                }
+              >
+                <ButtonIconCount src={cartIcon} count={qty} />
+                Adicionar ao carrinho
+              </Button>
+            </MovieCardActions>
+          </MovieCardRoot>
+        )
+      })}
+    </MoviesListWrapper>
   )
 }
-
-export default HomePage

1a16ce558b5ddfbbed4cfc9c06dc079a7f6a5243
developerlucaslima <developerlucaslima@gmail.com>
2025-08-15 02:15:50 -0300

    fix: update margin in EmptyCartCardRoot to improve layout consistency

--- src/app/modules/cart/components/empty-cart-card/empty-cart-card-styles.ts
+++ src/app/modules/cart/components/empty-cart-card/empty-cart-card-styles.ts
@@ -10,7 +10,7 @@ export const EmptyCartCardRoot = styled.section`
   flex-direction: column;
   align-items: center;
   gap: 24px;
-  margin: 0 16px 40px;
+  margin: 0 16px 143.64px;
 
   @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
     padding-bottom: 102px;

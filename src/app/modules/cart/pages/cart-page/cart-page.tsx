6e919fc29387676bb63e853738eaad809f51fe87
developerlucaslima <developerlucaslima@gmail.com>
2025-08-14 23:27:30 -0300

    feat: enhance CartPage component with structured cart list and total display

--- src/app/modules/cart/pages/cart-page/cart-page.tsx
+++ src/app/modules/cart/pages/cart-page/cart-page.tsx
@@ -1,7 +1,59 @@
-const CartPage = () => {
+import { ButtonRoot } from '@/app/ui/button/button'
+import {
+  selectCartTotalPrice,
+  useCart,
+} from '@/shared/stores/cart-store/cart-store'
+import { formatBRL } from '@/shared/utils/format/currency'
+
+import {
+  CartListFooter,
+  CartListFooterActions,
+  CartListFooterContent,
+  CartListHeader,
+  CartListHeaderCell,
+  CartListRoot,
+  CartListSeparator,
+} from '../../components/cart-list/cart-list'
+import { CartListItem } from '../../components/cart-list/cart-list-row'
+
+export const CartPage = () => {
+  const items = useCart((s) => s.items)
+  const selectedMovies = Object.values(items)
+  const total = useCart(selectCartTotalPrice)
+
   return (
-    <h1>Cart page</h1>
+    <CartListRoot>
+      <CartListHeader>
+        <CartListHeaderCell className="product">Produto</CartListHeaderCell>
+        <CartListHeaderCell className="qtySub">
+          <span>QTD</span>
+          <span>SUBTOTAL</span>
+        </CartListHeaderCell>
+        <CartListHeaderCell className="actions" />
+      </CartListHeader>
+
+      {selectedMovies.map((m) => (
+        <CartListItem
+          key={m.id}
+          id={m.id}
+          title={m.title}
+          image={m.image}
+          unitPrice={m.price}
+        />
+      ))}
+
+      <CartListSeparator />
+
+      <CartListFooter>
+        <CartListFooterActions>
+          <ButtonRoot>finalizar pedido</ButtonRoot>
+        </CartListFooterActions>
+
+        <CartListFooterContent>
+          <span className="label">total</span>
+          <strong className="value">{formatBRL(total)}</strong>
+        </CartListFooterContent>
+      </CartListFooter>
+    </CartListRoot>
   )
 }
-
-export default CartPage

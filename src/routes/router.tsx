f9dcb4f68555837fbf73e4c5b4559f5c09b42294
developerlucaslima <developerlucaslima@gmail.com>
2025-08-15 01:52:32 -0300

    feat: add empty-cart route and update router configuration

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -2,16 +2,20 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { MainLayout } from '@/app/layouts/app-layout/app-layout'
 import { CartPage } from '@/app/modules/cart/pages/cart-page/cart-page'
+import { EmptyCartPage } from '@/app/modules/cart/pages/empty-cart-page/empty-cart-page'
 import { CheckoutPage } from '@/app/modules/checkout/pages/checkout-page/checkout-page'
 import { HomePage } from '@/app/modules/home/pages/home-page/home-page'
+
+import { ROUTES } from './routes'
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: 'cart', element: <CartPage /> },
-      { path: 'checkout', element: <CheckoutPage /> },
+      { path: ROUTES.cart, element: <CartPage /> },
+      { path: ROUTES.checkout, element: <CheckoutPage /> },
+      { path: ROUTES['empty-cart'], element: <EmptyCartPage /> },
     ],
   },
 ])
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ export const ROUTES = {
   home: '/',
   cart: '/cart',
   checkout: '/checkout',
+  'empty-cart': '/empty-cart',
 } as const
 
 export type RouteKey = keyof typeof ROUTES
